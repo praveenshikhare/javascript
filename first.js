8da7f6c5ea6dd380cca62fcd0989a611c1c149ea
praveen_shikhare <praveenshikhare4@gmail.com>
2026-03-13 21:07:15 +0530

    Add files via upload
     types of data in javascript

---
+++ first.js
@@ -0,0 +1,16 @@
+const product ={
+    title:"ball pen",
+    ratting:4,
+    price:270,
+    offer: 5,
+};
+
+console.log(product);
+
+const profile={
+    user_name:"praveenshikhare",
+    posts:5,
+    following:260,
+    followers:280,
+};
+console.log(profile);
