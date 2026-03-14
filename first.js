4154610fe46750f3e0bdca4971d9efcc5078b769
praveen_shikhare <praveenshikhare4@gmail.com>
2026-03-14 18:04:00 +0530

    Add files via upload

--- first.js
+++ first.js
@@ -1,16 +1,9 @@
-const product ={
-    title:"ball pen",
-    ratting:4,
-    price:270,
-    offer: 5,
-};
+let number = prompt("enter a number")
+    if(number%5===0){
+        console.log(number,"is divisible by 5");
+    }
+    else{
+        console.log(number,"is not divisible by 5");
+    }
 
-console.log(product);
 
-const profile={
-    user_name:"praveenshikhare",
-    posts:5,
-    following:260,
-    followers:280,
-};
-console.log(profile);
