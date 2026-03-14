a84f04d9c455c84b323650184da07a6482c37a3f
praveen_shikhare <praveenshikhare4@gmail.com>
2026-03-14 19:20:06 +0530

    Add files via upload

--- first.js
+++ first.js
@@ -1,9 +1,20 @@
-let number = prompt("enter a number")
-    if(number%5===0){
-        console.log(number,"is divisible by 5");
-    }
-    else{
-        console.log(number,"is not divisible by 5");
-    }
-
+let score =prompt("enter your score");
+if(score>=80 && score<=100){
+    console.log("your grade is A");
+}
+else if(score>=70 && score<80){
+    console.log("your grade is B");
+}
+else if(score>=60 && score<70){
+    console.log("your grade is C");
+}
+else if(score>=50 && score<60){
+    console.log("your grade is D");
+}
+else if(score>=0 && score<50){
+    console.log("your grade is F");
+}
+else{
+    console.log("Invalid score");
+}
 
