6675869d9538ae99022a8e1f68070703e0048bac
praveen_shikhare <praveenshikhare4@gmail.com>
2026-03-19 12:42:54 +0530

    string.javascript

--- first.js
+++ first.js
@@ -1,20 +1,3 @@
-let score =prompt("enter your score");
-if(score>=80 && score<=100){
-    console.log("your grade is A");
-}
-else if(score>=70 && score<80){
-    console.log("your grade is B");
-}
-else if(score>=60 && score<70){
-    console.log("your grade is C");
-}
-else if(score>=50 && score<60){
-    console.log("your grade is D");
-}
-else if(score>=0 && score<50){
-    console.log("your grade is F");
-}
-else{
-    console.log("Invalid score");
-}
-
+let fullname=prompt("Enter your name");
+let username="@"+fullname+fullname.length;
+console.log(username);
