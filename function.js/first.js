312b660c803322f85d8711b62022148285586e41
praveen_shikhare <praveenshikhare4@gmail.com>
2026-03-20 16:49:28 +0530

    Add files via upload

---
+++ function.js/first.js
@@ -0,0 +1,10 @@
+function vowelscount(str){
+    let count=0;
+    for(let char of str){
+        if(char==="a" || char==="e" || char==="i" || char==="o" || char==="u"){
+            count++;
+        }
+    }
+    console.log(count);
+    
+}
