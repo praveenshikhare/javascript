9aa18c431ccdade6540ac5238de9ce8e3e9ce592
praveen_shikhare <praveenshikhare4@gmail.com>
2026-03-25 23:07:28 +0530

    Add files via upload

---
+++ dom2.js/first.js
@@ -0,0 +1,6 @@
+let newbtn =document.createElement("button");
+newbtn.innerText="click me!";
+newbtn.style.color="white";
+newbtn.style.backgroundColor="green";
+
+document.querySelector("body").prepend(newbtn);
