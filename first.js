52ef12c5ad7a9a5a95cce5488623058cc7c6d9b8
praveen_shikhare <praveenshikhare4@gmail.com>
2026-04-14 15:24:37 +0530

    Add files via upload

--- first.js
+++ first.js
@@ -1,5 +1,22 @@
 // console.log("i love js");
 // console.log("hello world");
 
-fullName ="praveen shikhare";
-console.log(fullName);
+// fullName ="praveen shikhare";
+// console.log(fullName);
+
+//  const student={
+//     fullName:"praveen shikhare",
+//     age:19,
+//     cgpa:8.6,
+//  }
+//  student["fullName"]="shweta patil";
+//  console.log(student.fullName);
+
+const profile={
+    fullName:"shradha khapra",
+    post:195,
+    follow:true,
+    likes:100
+}
+console.log(profile);
+
