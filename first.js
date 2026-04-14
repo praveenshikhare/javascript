7f09a974ff50c8c8d0c1498a4724563afd66d8ac
praveen_shikhare <praveenshikhare4@gmail.com>
2026-04-14 21:22:35 +0530

    Add files via upload

--- first.js
+++ first.js
@@ -12,11 +12,31 @@
 //  student["fullName"]="shweta patil";
 //  console.log(student.fullName);
 
-const profile={
-    fullName:"shradha khapra",
-    post:195,
-    follow:true,
-    likes:100
-}
-console.log(profile);
+// const profile={
+//     fullName:"shradha khapra",
+//     post:195,
+//     follow:true,
+//     likes:100
+// }
+// console.log(profile);
+
+//operrators 
+// let a=5;
+// let b=4;
+//  console.log("a+b =",a+b);
+//  console.log("a-b =",a-b);
+//  console.log("a*b =",a*b);
+//  console.log("a/b =",a/b);
+//  console.log("a%b =",a%b);
+//  console.log("a**b =",a**b);
+//  console.log("a++ =",a++);
+//  console.log("++a =",++a);
+
+// logical operators
+let a=5;
+let b=3;
+let cond1 =a<b;
+let cond2=b===3;
+console.log(cond1&&cond2);
+console.log(cond1||cond2);
 
